1a5840450118acda53e2053d0c216dc5ae0f202a
Nikita Starshinov <nstarshinov@yandex.ru>
2024-07-30 14:03:15 +0300

    Add explicit initializer

--- Sources/VariableBlur/VariableBlur.swift
+++ Sources/VariableBlur/VariableBlur.swift
@@ -20,6 +20,12 @@ public struct VariableBlurView: UIViewRepresentable {
     /// By default, variable blur starts from 0 blur radius and linearly increases to `maxBlurRadius`. Setting `startOffset` to a small negative coefficient (e.g. -0.1) will start blur from larger radius value which might look better in some cases.
     public var startOffset: CGFloat = 0
     
+    public init(maxBlurRadius: CGFloat = 20, direction: VariableBlurDirection = .blurredTopClearBottom, startOffset: CGFloat = 0) {
+        self.maxBlurRadius = maxBlurRadius
+        self.direction = direction
+        self.startOffset = startOffset
+    }
+    
     public func makeUIView(context: Context) -> VariableBlurUIView {
         VariableBlurUIView(maxBlurRadius: maxBlurRadius, direction: direction, startOffset: startOffset)
     }
